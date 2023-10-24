f98f0f9ca73f41513b1239854b689f5cb8348d29
Matt Harrison <iwasinnamuknow@genestate.com>
2023-10-24 17:32:38 +0100

    Make some header group includes & added to PCH

---
+++ include/core/core.hpp
@@ -0,0 +1,35 @@
+/*
+ * This file is part of rosa.
+ *
+ *  rosa is free software: you can redistribute it and/or modify it under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation, either version
+ *  3 of the License, or (at your option) any later version.
+ *
+ *  rosa is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with bbai. If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <core/components/LuaScriptComponent.hpp>
+#include <core/components/NativeScriptComponent.hpp>
+#include <core/components/SpriteComponent.hpp>
+#include <core/components/TransformComponent.hpp>
+
+#include <core/input/Keyboard.hpp>
+#include <core/input/Mouse.hpp>
+
+#include <core/Entity.hpp>
+#include <core/Event.hpp>
+#include <core/EventManager.hpp>
+#include <core/Exception.hpp>
+#include <core/GameManager.hpp>
+#include <core/NativeScriptEntity.hpp>
+#include <core/Resource.hpp>
+#include <core/ResourceManager.hpp>
+#include <core/Scene.hpp>
+#include <core/SceneSerialiser.hpp>
---
+++ include/graphics/graphics.hpp
@@ -0,0 +1,26 @@
+/*
+ * This file is part of rosa.
+ *
+ *  rosa is free software: you can redistribute it and/or modify it under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation, either version
+ *  3 of the License, or (at your option) any later version.
+ *
+ *  rosa is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with bbai. If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <graphics/Colour.hpp>
+#include <graphics/Drawable.hpp>
+#include <graphics/gl.hpp>
+#include <graphics/Rect.hpp>
+#include <graphics/RenderWindow.hpp>
+#include <graphics/Shader.hpp>
+#include <graphics/Sprite.hpp>
+#include <graphics/Texture.hpp>
+#include <graphics/Vertex.hpp>
