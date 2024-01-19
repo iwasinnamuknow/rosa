c2991a1d96e664ace15a2862935ef053783aee08
Matt Harrison <iwasinnamuknow@genestate.com>
2024-01-19 07:15:46 +0000

    Tidied up examples (docs and unused code)

--- examples/countdown.cpp
+++ examples/countdown.cpp
@@ -14,57 +14,53 @@
  */
 
 
-// Bring in everything
+// Rosa objects we'll need
 #include <core/GameManager.hpp>
 #include <core/components/LuaScriptComponent.hpp>
 #include <core/components/SoundPlayerComponent.hpp>
 
-using namespace std::literals;
-
-static constexpr auto script_uuid = rosa::Uuid("f611c6e3-6836-0333-978c-cfadd31a5bfa");
-
+// Script asset
+constexpr auto script_uuid = rosa::Uuid("f611c6e3-6836-0333-978c-cfadd31a5bfa");
+// Default countdown duration
 static int countdown_start{1200};
 
 
 // Create a class to represent our scene
 class CountdownScene : public rosa::Scene {
-    public:
+public:
 
-        // Pass default params to the base class constructor
-        explicit CountdownScene(rosa::RenderWindow* render_window) : rosa::Scene(render_window) {}
+    // Pass default params to the base class constructor
+    explicit CountdownScene(rosa::RenderWindow* render_window) : rosa::Scene(render_window) {
+        auto& parent = createEntity();
 
-        // Override the onLoad function so we can set up our scene. This will be called
-        // any time the GameManager activates the scene.
-        auto onLoad() -> void override {
-
-            auto& parent = createEntity();
-
-            for (int i = 0; i < 12; i++) {
-                auto& entity = createEntity();
-                entity.setParent(parent.getUUID());
-                auto& sprite = entity.addComponent<rosa::SpriteComponent>();
-                sprite.setBatched(true);
-            }
-
-            parent.addComponent<rosa::SoundPlayerComponent>();
-            auto& script_comp = parent.addComponent<rosa::LuaScriptComponent>(this, parent);
-            script_comp.setValue("start_time", countdown_start);
-            script_comp.setScript(script_uuid);
+        for (int i = 0; i < 12; i++) {
+            auto& entity = createEntity();
+            entity.setParent(parent.getUUID());
+            auto& sprite = entity.addComponent<rosa::SpriteComponent>();
+            sprite.setBatched(true);
         }
+
+        parent.addComponent<rosa::SoundPlayerComponent>();
+        auto& script_comp = parent.addComponent<rosa::LuaScriptComponent>(this, parent);
+        script_comp.setValue("start_time", countdown_start);
+        script_comp.setScript(script_uuid);
+    }
 };
 
+// Entrypoint
 auto main(int argc, char* argv[]) -> int {
 
+    // Parse arguments for countdown duration
     if (argc > 1) {
         try {
             countdown_start = std::stoi(std::string(argv[1]));
-        } catch (std::exception e) {
+        } catch (std::exception& e) {
             std::cout << "Error parsing time from argument, " << e.what() << "\n";
         }
     }
 
     // Grab the GameManager
-    auto game_mgr = rosa::GameManager(800, 600, "Coundown Timer");
+    auto game_mgr = rosa::GameManager(800, 600, "Countdown Timer");
 
     rosa::ResourceManager::getInstance().registerAssetPack("base.pak", "");
 
