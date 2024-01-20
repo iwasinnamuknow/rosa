35721fa3719c06e39f6f32cc012ea080e7ff1f7f
Matt Harrison <iwasinnamuknow@genestate.com>
2024-01-20 02:01:03 +0000

    Finish docs for ImageComparator

--- include/graphics/ImageComparator.hpp
+++ include/graphics/ImageComparator.hpp
@@ -13,56 +13,88 @@
  *  see <https://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 #include <vector>
 #include <lodepng.h>
 #include <core/Exception.hpp>
 
 namespace rosa {
 
+    /**
+     * \brief Used to compare graphics output with reference images
+     *
+     * Currently only supports very simple pixel-for-pixel equality test
+     */
     class ImageComparator {
-        
-        public:
+    public:
+        /**
+         * \brief Compare pixels of two images
+         *
+         * This compares pixels directly with each other with no intelligence or finesse.
+         *
+         * \param lhs image data
+         * \param rhs other image data
+         * \return true if all pixels match, false otherwise
+         */
         static auto compareEqualityBasic(std::span<const unsigned char> lhs, std::span<unsigned char> rhs) -> bool {
-                
-                if (lhs.size() != rhs.size()) {
+
+            // Images must be of equal size
+            if (lhs.size() != rhs.size()) {
+                return false;
+            }
+
+            auto size = lhs.size();
+
+            // Compare each pixel, very simple
+            for (
+                    std::size_t index = 0; index < size; index++
+                    ) {
+                if (lhs[index] != rhs[index]) {
                     return false;
                 }
-
-                auto size = lhs.size();
-
-                for (std::size_t index = 0; index < size; index++) {
-                    if (lhs[index] != rhs[index]) {
-                        return false;
-                    }
-                }
-
-                return true;
             }
 
-            static auto writePNG(const std::string& filename, std::vector<unsigned char>& image, unsigned width, unsigned height) -> void {
-                //Encode the image
-                unsigned error = lodepng::encode(filename, image, width, height);
+            return true;
+        }
 
-                //if there's an error, display it
-                if(error) {
-                    throw Exception(fmt::format("PNG encoder error in {}: {} ({})", filename, error, lodepng_error_text(error)));
-                }
+        /**
+         * \brief Write image data to a png file
+         * \param filename destination filename on disk
+         * \param image span of image data 32bpp RGBA
+         * \param width image width
+         * \param height image height
+         */
+        static auto writePNG(const std::string& filename, std::span<unsigned char> image, unsigned width, unsigned height) -> void {
+            //Encode the image
+            unsigned error = lodepng::encode(filename, image.data(), width, height);
+
+            //if there's an error, display it
+            if (error != 0) {
+                throw Exception(fmt::format("PNG encoder error in {}: {} ({})", filename, error, lodepng_error_text(error)));
+            }
+        }
+
+        /**
+         * \brief Load image data from an on-disk png file
+         * \param filename source filename on disk
+         * \return A vector of image data 32bpp RGBA
+         */
+        static auto readPNG(const std::string& filename) -> std::vector<unsigned char> {
+            std::vector<unsigned char> image; //the raw pixels
+            unsigned width{0};
+            unsigned height{0};
+
+            //decode
+            unsigned error = lodepng::decode(image, width, height, filename);
+
+            if (error != 0) {
+                throw Exception(fmt::format("PNG decoder error in {}: {} ({})", filename, error, lodepng_error_text(error)));
             }
 
-            static std::vector<unsigned char> readPNG(const std::string& filename) {
-                std::vector<unsigned char> image; //the raw pixels
-                unsigned width, height;
-
-                //decode
-                unsigned error = lodepng::decode(image, width, height, filename);
-
-                if(error) {
-                    throw Exception(fmt::format("PNG decoder error in {}: {} ({})", filename, error, lodepng_error_text(error)));
-                }
-
-                //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
-                return image;
-            }
+            //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+            return image;
+        }
 
     };
 
